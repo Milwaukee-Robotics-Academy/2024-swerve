69b36e8927f4564ad362788c165fdb4870be2662
Erik Lee <ErikSLee@gmail.com>
2024-03-08 12:15:59 -0600

    Playing with fusion (#8)
    * adding ToF
    
    * Intake Sensor
    
    * oob default
    
    * delay stop .3 seconds

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -59,6 +59,8 @@ public class RobotContainer
   // CommandJoystick driverController   = new CommandJoystick(3);//(OperatorConstants.DRIVER_CONTROLLER_PORT);
   CommandXboxController driverXbox = new CommandXboxController(0);
 
+  Trigger intakeHasNote = new Trigger(intake::hasNote);
+
   /**
    * The container for the robot. Contains subsystems, OI devices, and commands.
    */
@@ -121,6 +123,7 @@ public class RobotContainer
      driverXbox.y().whileTrue(new InstantCommand(()->intake.in()).handleInterrupt(() -> intake.stop()));
      driverXbox.y().onFalse(new InstantCommand(()->intake.stop()));
 
+     intakeHasNote.onTrue(new WaitCommand(.3).andThen(new InstantCommand(()->intake.stop())));
 //    new JoystickButton(driverXbox, 3).whileTrue(new RepeatCommand(new InstantCommand(drivebase::lock, drivebase)));
   }
 
