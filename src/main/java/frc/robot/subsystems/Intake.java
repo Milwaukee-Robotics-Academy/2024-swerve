a4851f313709a7b2fd37f4afde983cd18f290971
Erik Lee <ErikSLee@gmail.com>
2024-03-08 12:18:26 -0600

    Playing with fusion (#8) (#9)
    * adding ToF
    
    * Intake Sensor
    
    * oob default
    
    * delay stop .3 seconds

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -5,10 +5,13 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.playingwithfusion.TimeOfFlight;
+import com.playingwithfusion.TimeOfFlight.RangingMode;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkBase.IdleMode;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.IntakeConstants;
 import frc.robot.Constants.ShooterConstants;
@@ -16,7 +19,11 @@ import frc.robot.Constants.ShooterConstants;
 public class Intake extends SubsystemBase {
    private CANSparkMax m_leftIntake = new CANSparkMax(IntakeConstants.kLeftMotorPort, MotorType.kBrushless);
     private CANSparkMax m_rightIntake = new CANSparkMax(IntakeConstants.kRightMotorPort, MotorType.kBrushless);
-    
+    /**
+     * Adding a Playing with Fusion Time of Flight sensor. this is configured via a webserver running off the Rob0Rio http://10.88.02.2:5812
+     * 
+     */
+  private TimeOfFlight intakeSensor = new TimeOfFlight(0);
   /** Creates a new Intake. */
   public Intake() {
 
@@ -28,6 +35,8 @@ public class Intake extends SubsystemBase {
        m_rightIntake.setSmartCurrentLimit(20);
        m_rightIntake.setIdleMode(IdleMode.kBrake);
        m_rightIntake.setInverted(true);
+       intakeSensor.setRangingMode(RangingMode.Short,24);
+      SmartDashboard.putNumber("IntakeSensor",intakeSensorDistance());
      
 
   }
@@ -47,8 +56,21 @@ public class Intake extends SubsystemBase {
     m_rightIntake.set(0);
   }
 
+  public boolean hasNote(){
+    if ((int)intakeSensor.getRange() < 100){
+      return true;
+    }
+    return false;
+  }
+
+  public int intakeSensorDistance(){
+    return (int)intakeSensor.getRange();
+  }
+
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+          SmartDashboard.putNumber("IntakeSensor",intakeSensorDistance());
+
   }
 }
