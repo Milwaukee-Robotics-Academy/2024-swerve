a54cb6512ca1ef591119a4c2a574c1c30dec1939
Laptop2 <Laptop2@milwaukeeroboticsacademy.org>
2024-03-29 17:30:45 -0500

    working AO-1-AO-4-AO

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -171,7 +171,7 @@ public class RobotContainer
       // new Intake(shooter).withTimeout(.1)
     );
     NamedCommands.registerCommand("levelIntake",
-      new RunCommand(shooter::spitBackOut).until(() -> !shooter.hasNote()).withTimeout(0.8)
+      new RunCommand(shooter::spitBackOut).until(() -> !shooter.hasNote()).withTimeout(0.2)
     );
     NamedCommands.registerCommand("intakeStop",
       // new InstantCommand(shooter::stop)
